d8b2262dbbe6708343f0c15e36f3d03675080b76
V <sanjogrijal@hotmail.co.uk>
2024-12-22 23:01:57 +0545

    feat: csurf working

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as csurf from 'csurf';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(csurf);
-  await app.listen(3000);
+  app.use(cookieParser());
+  app.use(csurf({ cookie: { sameSite: true } }));
+  await app.listen(3001);
 }
 bootstrap();
