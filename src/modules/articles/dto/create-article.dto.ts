900e2d005e05e95a68260c0e378e26bd583fec3e
V <sanjogrijal@hotmail.co.uk>
2025-01-07 00:00:36 +0545

    feat: create articles dto

--- src/modules/articles/dto/create-article.dto.ts
+++ src/modules/articles/dto/create-article.dto.ts
@@ -1,1 +1,22 @@
-export class CreateArticleDto {}
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export default class CreateAuthDTO {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  author: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  content: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  coverImage: number;
+
+  @ApiPropertyOptional()
+  source?: string;
+}
