c4f1c231825a731f4b4b4718b4530ba5c831eacf
V <sanjogrijal@hotmail.co.uk>
2025-01-07 00:10:42 +0545

    feat: reorganize modules and fix import errors

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from '../user/repository/user.repository';
 import { UserService } from '../user/user.service';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { AuthController } from './controller/auth.controller';
 import AuthEntity from './entities/auth.entity';
 import AuthRepository from './repository/auth.repository';
+import { AuthService } from './service/auth.service';
 import { AccessTokenStrategy } from './strategies/accessToken.strategy';
 import { RefreshTokenStrategy } from './strategies/refreshToken.strategy';
-import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AuthEntity]), JwtModule.register({})],
